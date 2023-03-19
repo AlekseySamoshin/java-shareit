78dd52b39ba6a357a3d6a24a6023bd8a03734c2f
Aleksey Samoshin <samoshin.aleksey@yandex.ru>
2023-03-19 13:08:55 +0400

    fix: исправление зависимостей и проблем с запуском

--- src/main/java/ru/practicum/shareit/booking/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/BookingRepository.java
@@ -2,9 +2,11 @@ package ru.practicum.shareit.booking;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.List;
 import java.util.Optional;
+
 
 public interface BookingRepository extends JpaRepository<Booking, Long>, BookingRepositoryCustom {
 
