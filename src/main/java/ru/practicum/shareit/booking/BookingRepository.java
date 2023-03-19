ea343541c5a8beaf923a377f2ee6e9982a784d4c
Aleksey Samoshin <samoshin.aleksey@yandex.ru>
2023-03-19 13:13:19 +0400

    fix: исправление code style

--- src/main/java/ru/practicum/shareit/booking/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/BookingRepository.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.booking;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.List;
 import java.util.Optional;
