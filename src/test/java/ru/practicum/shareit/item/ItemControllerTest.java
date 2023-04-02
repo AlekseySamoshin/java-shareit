bb4a835f7daba5ec8b566bdf50140346a83c110d
Aleksey Samoshin <samoshin.aleksey@yandex.ru>
2023-04-03 01:30:50 +0400

    feat: добавить тесты ItemController

---
+++ src/test/java/ru/practicum/shareit/item/ItemControllerTest.java
@@ -0,0 +1,174 @@
+package ru.practicum.shareit.item;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.practicum.shareit.booking.BookingRepository;
+import ru.practicum.shareit.booking.BookingService;
+import ru.practicum.shareit.item.dto.CommentDto;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.user.UserRepository;
+
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ItemControllerTest {
+
+    @MockBean
+    private ItemService itemService;
+
+    @Autowired
+    ObjectMapper mapper;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private ItemDto itemDto;
+    private CommentDto commentDto;
+
+    @BeforeEach
+    void setUp() {
+        itemDto = new ItemDto();
+        itemDto.setId(1L);
+        itemDto.setName("itemDto");
+        itemDto.setDescription("item dto description");
+        itemDto.setAvailable(true);
+
+        commentDto = new CommentDto();
+        commentDto.setText("comment");
+        commentDto.setId(1L);
+        commentDto.setCreated(LocalDateTime.now().toString());
+        commentDto.setId(1L);
+        commentDto.setAuthorName("author");
+    }
+
+    @Test
+    void getItems() throws Exception {
+        when(itemService.getItemsByUserId(eq(1L))).thenReturn(List.of(itemDto));
+        when(itemService.getItemsByUserId(eq(1L), eq(1), eq(10))).thenReturn(List.of(itemDto));
+        mockMvc.perform(get("/items")
+                        .header("X-Sharer-User-Id", 1)
+                        .content(mapper.writeValueAsString(itemDto))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id", is(itemDto.getId()), Long.class));
+
+        mockMvc.perform(get("/items")
+                .header("X-Sharer-User-Id", 1)
+                .param("from", "1")
+                .param("size", "10")
+                .content(mapper.writeValueAsString(itemDto))
+                .characterEncoding(StandardCharsets.UTF_8)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id", is(itemDto.getId()), Long.class));
+    }
+
+    @Test
+    void getItemById() throws Exception {
+        when(itemService.getItemById(eq(1L), eq(1L))).thenReturn(itemDto);
+        mockMvc.perform(get("/items/{itemId}", itemDto.getId())
+                        .header("X-Sharer-User-Id", 1)
+                        .content(mapper.writeValueAsString(itemDto))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(itemDto.getId()), Long.class));
+    }
+
+    @Test
+    void searchItemsByText() throws Exception {
+            when(itemService.searchItemsByText(anyLong(), eq("search1"),eq(null), eq(null)))
+                    .thenReturn(List.of(itemDto));
+            when(itemService.searchItemsByText(eq(1L), eq("search2"), eq(1), eq(10)))
+                    .thenReturn(List.of(itemDto));
+            mockMvc.perform(get("/items/search")
+                            .header("X-Sharer-User-Id", 1)
+                            .param("text", "search1")
+                            .characterEncoding(StandardCharsets.UTF_8)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .accept(MediaType.APPLICATION_JSON))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$[0].id", is(itemDto.getId()), Long.class));
+
+        mockMvc.perform(get("/items/search")
+                        .header("X-Sharer-User-Id", 1)
+                        .param("text", "search2")
+                        .param("from", "1")
+                        .param("size", "10")
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id", is(itemDto.getId()), Long.class));
+    }
+
+    @Test
+    void addItem() throws Exception {
+        when(itemService.addItem(eq(1L), any())).thenReturn(itemDto);
+        mockMvc.perform(post("/items")
+                        .header("X-Sharer-User-Id", 1)
+                        .content(mapper.writeValueAsString(itemDto))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(itemDto.getId()), Long.class));
+    }
+
+    @Test
+    void addComment() throws Exception {
+        when(itemService.addNewComment(eq(1L), eq(itemDto.getId()), any())).thenReturn(commentDto);
+        mockMvc.perform(post("/items/{itemId}/comment", itemDto.getId())
+                        .header("X-Sharer-User-Id", 1)
+                        .content(mapper.writeValueAsString(commentDto))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(commentDto.getId()), Long.class));
+    }
+
+    @Test
+    void updateItem() throws Exception {
+        when(itemService.updateItem(eq(1L),eq(1L), any())).thenReturn(itemDto);
+        mockMvc.perform(patch("/items/{itemId}", itemDto.getId())
+                        .header("X-Sharer-User-Id", 1)
+                        .content(mapper.writeValueAsString(itemDto))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(itemDto.getId()), Long.class));
+    }
+}
