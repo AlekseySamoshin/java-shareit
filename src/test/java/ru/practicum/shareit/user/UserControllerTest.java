79572b88e5bc3010b0c1a1d9c7c9ec79c1a98b7e
Aleksey Samoshin <samoshin.aleksey@yandex.ru>
2023-04-02 13:43:43 +0400

    feat: добавить тесты UserService и UserController

--- src/test/java/ru/practicum/shareit/user/UserControllerTest.java
+++ src/test/java/ru/practicum/shareit/user/UserControllerTest.java
@@ -12,19 +12,19 @@ import org.springframework.test.web.servlet.MockMvc;
 import ru.practicum.shareit.user.dto.UserDto;
 
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 @SpringBootTest
 @AutoConfigureMockMvc
-//@WebMvcTest(controllers = UserController.class)
 public class UserControllerTest {
     @MockBean
     private UserService userService;
@@ -35,60 +35,107 @@ public class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    private UserDto userDto;
+    private UserDto user1Dto;
+    private UserDto user2Dto;
 
     @BeforeEach
     void setUp() {
-        userDto = new UserDto();
-        userDto.setId(1L);
-        userDto.setName("user1");
-        userDto.setEmail("test1@email.ru");
+        user1Dto = new UserDto();
+        user2Dto = new UserDto();
+        user1Dto.setId(1L);
+        user1Dto.setName("user1");
+        user1Dto.setEmail("test1@email.ru");
+        user2Dto.setId(2L);
+        user2Dto.setName("user2");
+        user2Dto.setEmail("test2@email.ru");
     }
 
     @Test
     void addUser() throws Exception {
         when(userService.addUser(any()))
-                .thenReturn(userDto);
+                .thenReturn(user1Dto);
 
         mockMvc.perform(post("/users")
-                        .content(mapper.writeValueAsString(userDto))
+                        .content(mapper.writeValueAsString(user1Dto))
                         .characterEncoding(StandardCharsets.UTF_8)
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id", is(userDto.getId()), Long.class))
-                .andExpect(jsonPath("$.name", is(userDto.getName())))
-                .andExpect(jsonPath("$.email", is(userDto.getEmail())));
+                .andExpect(jsonPath("$.id", is(user1Dto.getId()), Long.class))
+                .andExpect(jsonPath("$.name", is(user1Dto.getName())))
+                .andExpect(jsonPath("$.email", is(user1Dto.getEmail())));
     }
 
     @Test
     void updateUser() throws Exception {
         Long userId = 1L;
         when(userService.updateUser(anyLong(), any(UserDto.class)))
-                .thenReturn(userDto);
+                .thenReturn(user1Dto);
 
         mockMvc.perform(patch("/users/{userId}", userId)
-                        .content(mapper.writeValueAsString(userDto))
+                        .content(mapper.writeValueAsString(user1Dto))
                         .characterEncoding(StandardCharsets.UTF_8)
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id", is(userDto.getId()), Long.class))
-                .andExpect(jsonPath("$.name", is(userDto.getName())))
-                .andExpect(jsonPath("$.email", is(userDto.getEmail())));
+                .andExpect(jsonPath("$.id", is(user1Dto.getId()), Long.class))
+                .andExpect(jsonPath("$.name", is(user1Dto.getName())))
+                .andExpect(jsonPath("$.email", is(user1Dto.getEmail())));
     }
 
     @Test
-    void getUsers() {
+    void getUsers() throws Exception {
+        when(userService.getUsers())
+                .thenReturn(List.of(user1Dto, user2Dto));
+
+        mockMvc.perform(get("/users")
+                        .content(mapper.writeValueAsString(user1Dto))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[1].id", is(user2Dto.getId()), Long.class))
+                .andExpect(jsonPath("$[1].name", is(user2Dto.getName())))
+                .andExpect(jsonPath("$[1].email", is(user2Dto.getEmail())));
     }
 
     @Test
-    void getUserById() {
+    void getUserById() throws Exception {
+        Long userId = 1L;
+        when(userService.getUserById(anyLong()))
+                .thenReturn(user1Dto);
+
+        mockMvc.perform(get("/users/{userId}", userId)
+                        .content(mapper.writeValueAsString(user1Dto))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(user1Dto.getId()), Long.class))
+                .andExpect(jsonPath("$.name", is(user1Dto.getName())))
+                .andExpect(jsonPath("$.email", is(user1Dto.getEmail())));
     }
 
     @Test
-    void deleteUser() {
+    void deleteUser() throws Exception {
+        Long userId = 1L;
+        when(userService.deleteUserById(anyLong()))
+                .thenReturn(user1Dto);
+
+        mockMvc.perform(delete("/users/{userId}", userId)
+                        .content(mapper.writeValueAsString(user1Dto))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(user1Dto.getId()), Long.class))
+                .andExpect(jsonPath("$.name", is(user1Dto.getName())))
+                .andExpect(jsonPath("$.email", is(user1Dto.getEmail())));
     }
 }
---
+++ src/test/java/ru/practicum/shareit/user/UserServiceTest.java
@@ -0,0 +1,114 @@
+package ru.practicum.shareit.user;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import ru.practicum.shareit.exception.WrongDataException;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.dto.UserDtoMapper;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@ExtendWith(SpringExtension.class)
+class UserServiceTest {
+
+    @Mock
+    WrongDataException wrongDataException;
+
+    @Mock
+    UserRepository userRepository;
+
+    @Mock
+    UserDtoMapper userDtoMapper;
+
+    @InjectMocks
+    UserService userService;
+
+    private User user1;
+    private User user2;
+    private UserDto userDto1;
+    private UserDto userDto2;
+
+    @BeforeEach
+    void setup() {
+        user1 = new User();
+        user2 = new User();
+        userDto1 = new UserDto();
+        userDto2 = new UserDto();
+        user1.setId(1L);
+        user1.setName("userName1");
+        user1.setEmail("user1@email.com");
+        user2.setId(2L);
+        user2.setName("userName2");
+        user2.setEmail("user1@email.com");
+        userDto1.setId(1L);
+        userDto1.setName("userName1");
+        userDto1.setEmail("user1@email.com");
+        userDto2.setId(2L);
+        userDto2.setName("userName2");
+        userDto2.setEmail("user1@email.com");
+    }
+
+    @Test
+    void getUsers() {
+        List<User> users = List.of(user1);
+        when(userRepository.findAll()).thenReturn(users);
+        when(userDtoMapper.mapUser(any())).thenReturn(userDto1);
+        List<UserDto> testList = userService.getUsers();
+        assertEquals(testList.get(0).getId(), users.get(0).getId());
+    }
+
+
+    @Test
+    void getUserById() {
+        when(userRepository.findById(anyLong())).thenReturn(Optional.of(user1));
+        when(userDtoMapper.mapUser(any())).thenReturn(userDto1);
+
+        UserDto testUser = userService.getUserById(1L);
+        assertEquals(testUser.getId(), user1.getId());
+        assertEquals(testUser.getName(), user1.getName());
+        assertEquals(testUser.getEmail(), user1.getEmail());
+    }
+
+    @Test
+    void addUser() {
+        when(userRepository.save(any())).thenReturn(user1);
+        when(userDtoMapper.mapUser(any())).thenReturn(userDto1);
+
+        UserDto testUser = userService.addUser(userDto1);
+        assertEquals(testUser.getId(), userDto1.getId());
+        assertEquals(testUser.getName(), user1.getName());
+        assertEquals(testUser.getEmail(), user1.getEmail());
+    }
+
+    @Test
+    void addUserWrongData() {
+        userDto1.setName("");
+        when(userRepository.save(any())).thenReturn(user1);
+        when(userDtoMapper.mapUser(any())).thenReturn(userDto1);
+
+        assertThrows(WrongDataException.class, () -> userService.addUser(userDto1));
+    }
+
+    @Test
+    void updateUser() {
+        when(userRepository.save(any())).thenReturn(user1);
+        when(userRepository.findById(anyLong())).thenReturn(Optional.of(user1));
+        when(userDtoMapper.mapUser(any())).thenReturn(userDto1);
+
+        UserDto testUser = userService.updateUser(1L, userDto1);
+        assertEquals(testUser.getId(), userDto1.getId());
+        assertEquals(testUser.getName(), user1.getName());
+        assertEquals(testUser.getEmail(), user1.getEmail());
+    }
+}
