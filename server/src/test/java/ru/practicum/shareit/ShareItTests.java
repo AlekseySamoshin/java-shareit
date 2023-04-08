3cd47b89147a496ada5aa80a8bc1f4e9e570e5f2
Aleksey Samoshin <samoshin.aleksey@yandex.ru>
2023-04-08 19:10:05 +0400

    fix: исправить недочеты

--- server/src/test/java/ru/practicum/shareit/ShareItTests.java
+++ server/src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -1,9 +1,6 @@
 package ru.practicum.shareit;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
